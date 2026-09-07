cf8bb2216ec06d02756a1945395950ebe867babf
gwjp19 <gwjporter@icloud.com>
2026-09-07 14:46:56 -0400

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -3,4 +3,4 @@ n = len(fga1_list)
 mean = sum(fga1_list) / n
 squarred_diff = [(x-men) ** 2 for x in fga1_list]
 variance_pop = sum(squared_dif) / n
-sd_op = variance_pop ** 0.5
+AFGA = variance_pop ** 0.5
