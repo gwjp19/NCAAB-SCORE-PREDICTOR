aa6bbbb75076877d6929eb6e0252b27afe19b489
gwjp19 <gwjporter@icloud.com>
2026-09-07 15:21:40 -0400

    Update stat_stuff.py

--- stat_stuff.py
+++ stat_stuff.py
@@ -2,5 +2,5 @@ from Data_inputs import fga1_list
 n = len(fga1_list)
 mean = sum(fga1_list) / n
 squarred_diff = [(x-mean) ** 2 for x in fga1_list]
-variance_pop = sum(squared_dif) / n
+variance_pop = sum(squared_diff) / n
 sd_pop = variance_pop ** 0.5
