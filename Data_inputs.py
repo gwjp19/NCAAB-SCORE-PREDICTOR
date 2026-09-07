62473c0d75a9911d6db99e78eb7087df40810cf6
gwjp19 <gwjporter@icloud.com>
2026-09-07 12:44:07 -0400

    Update and rename Data inputs to Data_inputs.py

---
+++ Data_inputs.py
@@ -0,0 +1,5 @@
+#still_messy_still_working_on_it
+name1 = input("Team Name:")
+print("...")
+print(now for the FGA from the last five games " +name1+" played")
+fga_game1 = float(input("Game One FGA:")
