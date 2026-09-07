da34ca166950248dfe8f4b888e0f5e5136f3f81f
gwjp19 <gwjporter@icloud.com>
2026-09-07 13:18:12 -0400

    Update Data_inputs.py

--- Data_inputs.py
+++ Data_inputs.py
@@ -1,226 +1,52 @@
-#still_messy_still_working_on_it
-#will be automated soon
 name1 = input("Team Name:")
+fga1_list = []
+for i in range(5):
+  fga1 = float(input(name1+" Fga for the last five games:")
+  fga1_list.append(fga1)
+#/////////              
+fgaa1_list = []
+for i in range(5):
+  fgaa1 = float(input(name1+" Fga Allowed for the last five games:")
+  fgaa1_list.append(fgaa1)
+#/////////
+fd1_list = []
+for i in range(5):
+  fd1 = float(input(name1+" Fouls drawn for the last five games:")
+  fd1_list.append(fd1)
+#/////////
+fc1_list = []
+for i in range(5):
+  fc1 = float(input(name1+" Fouls commited for the last five games:")
+  fc1_list.append(fc1)
+#//////////         
+tv1_list = []
+for i in range(5):
+  tv1 = float(input(name1+" Turnovers for the last five games:")
+  tv1_list.append(tv1)
+#//////////  
+tvf1_list = []
+for i in range(5):
+  tvf1 = float(input(name1+" Turnovers forced for the last five games:")
+  tvf1_list.append(tvf1)
+#////////// 
+orb1_list = []
+for i in range(5):
+  orb1 = float(input("Offensive rebounds for the last five games:")
+  orb1_list.append(orb1)
+#//////////     
+drb1_list = []
+for i in range(5):
+  drb1 = float(input("Defensive rebounds for the last five games:")
+  drb1_list.append(drb1)
+#//////////
+drba1_list = []
+for i in range(5):
+  drba1 = float(input("Defensive rebounds allowed for the last five games:")
+  drba1_list.append(drba1)
+#//////////
+orba1_list = []
+for i in range(5):
+  orba1 = float(input("Offensive rebounds allowed for the last five games:")
+  orba1_list.append(orba1)
+#//////////
 
-print("...")
-
-print("now for the FGA from the last five games " +name1+" played")
-
-fga_game1 = float(input("Game One FGA:")
-fga_game2 = float(input("Game Two FGA:")
-fga_game3 = float(input("Game Three FGA:")
-fga_game4 = float(input("Game Four FGA:")
-fga_game5 = float(input("Game Five FGA:")
-#///////////////////
-                 
-print("now for the FGA Allowed from the last five games " +name1+" played")
-
-
-fgaa_game1 = float(input("Game One FGA Allowed:")
-fgaa_game2 = float(input("Game Two FGA Allowed:")
-fgaa_game3 = float(input("Game Three FGA Allowed:")
-fgaa_game4 = float(input("Game Four FGA Allowed:")
-fgaa_game5 = float(input("Game Five FGA Allowed:")  
-#//////////////////                
-
-print("now for the Fouls Drawn from the last five games " +name1+" played")
-
-
-fd_game1 = float(input("Game One Fouls Drawn:")
-fd_game2 = float(input("Game Two Fouls Drawn:")
-fd_game3 = float(input("Game Three Fouls Drawn:")
-fd_game4 = float(input("Game Four Fouls Drawn:")
-fd_game5 = float(input("Game Five Fouls Drawn:")  
-#//////////////////                
-
-print("now for the Fouls Commited from the last five games " +name1+" played")
-
-fc_game1 = float(input("Game One Fould Commited:")
-fc_game2 = float(input("Game Two Fouls Commited:")
-fc_game3 = float(input("Game Three Fouls Commited:")
-fc_game4 = float(input("Game Four Fouls Commited:")
-fc_game5 = float(input("Game Five Fouls Commited:")  
-#//////////////////                
-
-print("now for the ORB from the last five games " +name1+" played")
-
-
-orb_game1 = float(input("Game One ORB:")
-orb_game2 = float(input("Game Two ORB:")
-orb_game3 = float(input("Game Three ORB:")
-orb_game4 = float(input("Game Four ORB:")
-orb_game5 = float(input("Game Five ORB:")  
-#//////////////////                
-
-print("now for the DRB Allowed from the last five games " +name1+" played")
-
-
-drb_game1 = float(input("Game One DRB:")
-drb_game2 = float(input("Game Two DRB:")
-drb_game3 = float(input("Game Three DRB:")
-drb_game4 = float(input("Game Four DRB:")
-drb_game5 = float(input("Game Five DRB:")  
-#//////////////////                
-
-print("now for the ORB Allowed from the last five games " +name1+" played")
-
-
-orba_game1 = float(input("Game One ORB Allowed:")
-orba_game2 = float(input("Game Two ORB Allowed:")
-orba_game3 = float(input("Game Three ORB Allowed:")
-orba_game4 = float(input("Game Four ORB Allowed:")
-orba_game5 = float(input("Game Five ORB Allowed:")  
-#//////////////////                
-
-print("now for the DRB Allowed from the last five games " +name1+" played")
-
-
-drba_game1 = float(input("Game One DRB Allowed:")
-drba_game2 = float(input("Game Two DRB Allowed:")
-drba_game3 = float(input("Game Three DRB Allowed:")
-drba_game4 = float(input("Game Four DRB Allowed:")
-drba_game5 = float(input("Game Five DRB Allowed:")  
-#//////////////////                
-
-print("now for the TOV from the last five games " +name1+" played")
-
-
-tov_game1 = float(input("Game One TOV:")
-tov_game2 = float(input("Game Two TOV:")
-tov_game3 = float(input("Game Three TOV:")
-tov_game4 = float(input("Game Four TOV:")
-tov_game5 = float(input("Game Five TOV:")  
-#//////////////////                
-
-print("now for the TOV Forced from the last five games " +name1+" played")
-
-
-tovf_game1 = float(input("Game One TOVF:")
-tovf_game2 = float(input("Game Two TOVF:")
-tovf_game3 = float(input("Game Three TOVF:")
-tovf_game4 = float(input("Game Four TOVF:")
-tovf_game5 = float(input("Game Five TOVF:")  
-#////////////////// 
-
-print("now for the Adjusted Points Per Possesion for " + name1)
-
-ppp1 = float(input("PPP:")  
-#//////////////////       
-
-print("now for the Adjusted Points Allowed Per Possesion for " + name1)
-
-pppa1 = float(input("PPPA:")  
-#////////////////// 
-
-#team 2
-
-name2 = input("Team Name:")
-
-print("...")
-
-print("now for the FGA from the last five games " +name2+" played")
-
-fga2_game1 = float(input("Game One FGA:")
-fga2_game2 = float(input("Game Two FGA:")
-fga2_game3 = float(input("Game Three FGA:")
-fga2_game4 = float(input("Game Four FGA:")
-fga2_game5 = float(input("Game Five FGA:")
-#///////////////////
-                 
-print("now for the FGA Allowed from the last five games " +name2+" played")
-
-
-fgaa2_game1 = float(input("Game One FGA Allowed:")
-fgaa2_game2 = float(input("Game Two FGA Allowed:")
-fgaa2_game3 = float(input("Game Three FGA Allowed:")
-fgaa2_game4 = float(input("Game Four FGA Allowed:")
-fgaa2_game5 = float(input("Game Five FGA Allowed:")  
-#//////////////////                
-
-print("now for the Fouls Drawn from the last five games " +name2+" played")
-
-
-fd2_game1 = float(input("Game One Fouls Drawn:")
-fd2_game2 = float(input("Game Two Fouls Drawn:")
-fd2_game3 = float(input("Game Three Fouls Drawn:")
-fd2_game4 = float(input("Game Four Fouls Drawn:")
-fd2_game5 = float(input("Game Five Fouls Drawn:")  
-#//////////////////                
-
-print("now for the Fouls Commited from the last five games " +name2+" played")
-
-fc2_game1 = float(input("Game One Fould Commited:")
-fc2_game2 = float(input("Game Two Fouls Commited:")
-fc2_game3 = float(input("Game Three Fouls Commited:")
-fc2_game4 = float(input("Game Four Fouls Commited:")
-fc2_game5 = float(input("Game Five Fouls Commited:")  
-#//////////////////                
-
-print("now for the ORB from the last five games " +name2+" played")
-
-
-orb2_game1 = float(input("Game One ORB:")
-orb2_game2 = float(input("Game Two ORB:")
-orb2_game3 = float(input("Game Three ORB:")
-orb2_game4 = float(input("Game Four ORB:")
-orb2_game5 = float(input("Game Five ORB:")  
-#//////////////////                
-
-print("now for the DRB Allowed from the last five games " +name2+" played")
-
-
-drb2_game1 = float(input("Game One DRB:")
-drb2_game2 = float(input("Game Two DRB:")
-drb2_game3 = float(input("Game Three DRB:")
-drb2_game4 = float(input("Game Four DRB:")
-drb2_game5 = float(input("Game Five DRB:")  
-#//////////////////                
-
-print("now for the ORB Allowed from the last five games " +name2+" played")
-
-
-orba2_game1 = float(input("Game One ORB Allowed:")
-orba2_game2 = float(input("Game Two ORB Allowed:")
-orba2_game3 = float(input("Game Three ORB Allowed:")
-orba2_game4 = float(input("Game Four ORB Allowed:")
-orba2_game5 = float(input("Game Five ORB Allowed:")  
-#//////////////////                
-
-print("now for the DRB Allowed from the last five games " +name2+" played")
-
-
-drba2_game1 = float(input("Game One DRB Allowed:")
-drba2_game2 = float(input("Game Two DRB Allowed:")
-drba2_game3 = float(input("Game Three DRB Allowed:")
-drba2_game4 = float(input("Game Four DRB Allowed:")
-drba2_game5 = float(input("Game Five DRB Allowed:")  
-#//////////////////                
-
-print("now for the TOV from the last five games " +name2+" played")
-
-
-tov2_game1 = float(input("Game One TOV:")
-tov2_game2 = float(input("Game Two TOV:")
-tov2_game3 = float(input("Game Three TOV:")
-tov2_game4 = float(input("Game Four TOV:")
-tov2_game5 = float(input("Game Five TOV:")  
-#//////////////////                
-
-print("now for the TOV Forced from the last five games " +name2+" played")
-
-
-tovf2_game1 = float(input("Game One TOVF:")
-tovf2_game2 = float(input("Game Two TOVF:")
-tovf2_game3 = float(input("Game Three TOVF:")
-tovf2_game4 = float(input("Game Four TOVF:")
-tovf2_game5 = float(input("Game Five TOVF:")  
-#////////////////// 
-
-print("now for the Adjusted Points Per Possesion for " + name2)
-
-ppp2 = float(input("PPP:")  
-#//////////////////       
-
-print("now for the Adjusted Points Allowed Per Possesion for " + name2)
-
-pppa2 = float(input("PPPA:")              
