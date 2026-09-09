9b8ceec4a1970d8f34ae84d136b7983a8f4e8025
gwjp19 <gwjporter@icloud.com>
2026-09-09 10:45:49 -0400

    Update Data_inputs.py

--- Data_inputs.py
+++ Data_inputs.py
@@ -31,24 +31,75 @@ for i in range(5):
 #////////// 
 orb1_list = []
 for i in range(5):
-  orb1 = float(input("Offensive rebounds for the last five games:"))
+  orb1 = float(input(name1+" Offensive rebounds for the last five games:"))
   orb1_list.append(orb1)
 #//////////     
 drb1_list = []
 for i in range(5):
-  drb1 = float(input("Defensive rebounds for the last five games:"))
+  drb1 = float(input(name1+" Defensive rebounds for the last five games:"))
   drb1_list.append(drb1)
 #//////////
 drba1_list = []
 for i in range(5):
-  drba1 = float(input("Defensive rebounds allowed for the last five games:"))
+  drba1 = float(input(name1+" Defensive rebounds allowed for the last five games:"))
   drba1_list.append(drba1)
 #//////////
 orba1_list = []
 for i in range(5):
-  orba1 = float(input("Offensive rebounds allowed for the last five games:"))
+  orba1 = float(input(name1+" Offensive rebounds allowed for the last five games:"))
   orba1_list.append(orba1)
 #//////////
 
-print(fga1_list)
+#TEAM 2 //////////
 
+name2 = input("Team Name:")
+fga2_list = []
+for i in range(5):
+  fga2 = float(input(name2+" Fga for the last five games:"))
+  fga2_list.append(fga2)
+#/////////              
+fgaa2_list = []
+for i in range(5):
+  fgaa2 = float(input(name2+" Fga Allowed for the last five games:"))
+  fgaa2_list.append(fgaa2)
+#/////////
+fd2_list = []
+for i in range(5):
+  fd2 = float(input(name2+" Fouls drawn for the last five games:"))
+  fd2_list.append(fd2)
+#/////////
+fc2_list = []
+for i in range(5):
+  fc2 = float(input(name2+" Fouls commited for the last five games:"))
+  fc2_list.append(fc2)
+#//////////         
+tv2_list = []
+for i in range(5):
+  tv2 = float(input(name2+" Turnovers for the last five games:"))
+  tv2_list.append(tv2)
+#//////////  
+tvf2_list = []
+for i in range(5):
+  tvf2 = float(input(name2+" Turnovers forced for the last five games:"))
+  tvf2_list.append(tvf2)
+#////////// 
+orb2_list = []
+for i in range(5):
+  orb2 = float(input(nam2 +" Offensive rebounds for the last five games:"))
+  orb2_list.append(orb2)
+#//////////     
+drb2_list = []
+for i in range(5):
+  drb2 = float(input(name2 +" Defensive rebounds for the last five games:"))
+  drb2_list.append(drb2)
+#//////////
+drba2_list = []
+for i in range(5):
+  drba2 = float(input(name2 +" Defensive rebounds allowed for the last five games:"))
+  drba2_list.append(drba2)
+#//////////
+orba2_list = []
+for i in range(5):
+  orba2 = float(input(name2 + " Offensive rebounds allowed for the last five games:"))
+  orba2_list.append(orba2)
+#//////////
