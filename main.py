d383be4ec0223ffe38a6c9a34c006fc7fa5b7eda
gwjp19 <gwjporter@icloud.com>
2026-09-08 20:06:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -29,3 +29,68 @@ for z in z_values:
 fd1n=len(sim_fd1)
 fd1_mean = sum(sim_fd1) / fd1n
 print("Simulated Fouls Drawn Average is: " + str(fd1_mean))
+#///////////
+sim_fc1 =[]
+for z in z_values:
+  pfc1 = fc1mean + (fc1_sd_pop * z)
+  sim_fc1.append(pfc1)
+fc1n=len(sim_fc1)
+fc1_mean = sum(sim_fc1) / fc1n
+print("Simulated Fouls Drawn Average is: " + str(fc1_mean))
+#///////////
+sim_tv1 =[]
+for z in z_values:
+  ptv1 = tv1mean + (tv1_sd_pop * z)
+  sim_tv1.append(ptv1)
+tv1n=len(sim_tv1)
+tv1_mean = sum(sim_tv1) / tv1n
+print("Simulated Fouls Drawn Average is: " + str(tv1_mean))
+#///////////
+sim_tvf1 =[]
+for z in z_values:
+  ptvf1 = tvf1mean + (tvf1_sd_pop * z)
+  sim_tvf1.append(ptvf1)
+tvf1n=len(sim_tvf1)
+tvf1_mean = sum(sim_tvf1) / tvf1n
+print("Simulated Fouls Drawn Average is: " + str(tvf1_mean))
+#///////////
+sim_orb1 =[]
+for z in z_values:
+  porb1 = orb1mean + (orb1_sd_pop * z)
+  sim_orb1.append(porb1)
+orb1n=len(sim_orb1)
+orb1_mean = sum(sim_orb1) / orb1n
+print("Simulated Fouls Drawn Average is: " + str(orb1_mean))
+#///////////
+sim_orba1 =[]
+for z in z_values:
+  porba1 = orba1mean + (orba1_sd_pop * z)
+  sim_orba1.append(porba1)
+orba1n=len(sim_orba1)
+orba1_mean = sum(sim_orba1) / orba1n
+print("Simulated Fouls Drawn Average is: " + str(orba1_mean))
+#///////////
+sim_drb1 =[]
+for z in z_values:
+  pdrb1 = drb1mean + (drb1_sd_pop * z)
+  sim_drb1.append(drb1)
+drb1n=len(sim_drb1)
+drb1_mean = sum(sim_drb1) / drb1n
+print("Simulated Fouls Drawn Average is: " + str(drb1_mean))
+#///////////
+sim_drba1 =[]
+for z in z_values:
+  pdrba1 = drba1mean + (drba1_sd_pop * z)
+  sim_drba1.append(drba1)
+drba1n=len(sim_drba1)
+drba1_mean = sum(sim_drba1) / drba1n
+print("Simulated Fouls Drawn Average is: " + str(drba1_mean))
+#///////////
+
+
+
+
+
+
+
+
