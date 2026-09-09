b0a865e35a0102ad9a2241309db41768c9a26a70
gwjp19 <gwjporter@icloud.com>
2026-09-09 10:42:27 -0400

    Update math_stuff.py

--- math_stuff.py
+++ math_stuff.py
@@ -1,1 +1,7 @@
-from main.py import
+from main.py import pfga1, sim_fga1
+fga1_results = []
+for pfga1 in sim_fga1:
+  fga1_result = ( pfga1 + (1.2 * pfgaa2) / 2.2)
+  fga1_results.append(fga1_result)
+  
+  
