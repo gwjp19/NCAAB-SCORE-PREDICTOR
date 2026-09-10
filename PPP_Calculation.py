1820194c997dbc32e5e063c1041178d0270fe07b
gwjp19 <gwjporter@icloud.com>
2026-09-10 16:09:14 -0400

    Update PPP_Calculation.py

--- PPP_Calculation.py
+++ PPP_Calculation.py
@@ -1,1 +1,9 @@
+from data_imports.py import ppp1, papp1, ppp2, papp2
+
+ppp1_per_100 = (ppp1 / 100)
+papp1_per_100 = (papp1 / 100)
+ppp2_per_100 = (ppp2 / 100)
+papp2_per_100 = (papp2 / 100)
+
+adj_ppp1 = 
 
