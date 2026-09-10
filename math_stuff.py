1d65662349bf716c7354cdefb1c4d9d64ae6f495
gwjp19 <gwjporter@icloud.com>
2026-09-10 12:20:41 -0400

    Update math_stuff.py

--- math_stuff.py
+++ math_stuff.py
@@ -1,4 +1,5 @@
-from main.py import pfga1, sim_fga1, pfga2, sim_fga2, pfd1, sim_fd1, pfd2, sim_fd1, pfc1, sim_pfc1, pfc2, sim_pfc2, porb1, sim_orb1, porb2, sim_orb2, pdrb1, sim_drb1, pdrb2, sim_drb2, porba1, sim_orba1, porba2, sim_orba2. pdrba1, sim_drba1, pdrba1, sim_drba2
+from main.py import pfga1, sim_fga1, pfga2, sim_fga2, pfd1, sim_fd1, pfd2, sim_fd1, ptv1, sim_tv1, ptv2, sim_tv2, ptvf1, sim_tvf1, ptvf2, sim_tvf2
+from main.py import pfc1, sim_pfc1, pfc2, sim_pfc2, porb1, sim_orb1, porb2, sim_orb2, pdrb1, sim_drb1, pdrb2, sim_drb2, porba1, sim_orba1, porba2, sim_orba2. pdrba1, sim_drba1, pdrba1, sim_drba2
 fga1_results = []
 for pfga1 in sim_fga1:
   fga1_result = ( pfga1 + (1.2 * pfgaa2) / 2.2)
@@ -30,3 +31,16 @@ for porb2 in sim_orb2:
   porb2_result = (orb2_percent * ( porb2 + pdrba1)
   porb2_results.append(porb2_result)
 #////////////
+ptv1_results = []
+for ptv1 in sim_tv1:
+  ptv1_result = ((1.6 * ptv1)+(1.4 * ptvf2)) / 3
+  ptv1_results.append(ptv1_result)
+#////////////
+ptv2_results = []
+for ptv2 in sim_tv2:
+  ptv2_result = ((1.6 * ptv2)+(1.4 * ptvf1)) / 3
+  ptv2_results.append(ptv2_result)
+#////////////
+
+P_possesions =[]
+
